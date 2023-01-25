f77f842ac6c5b9718383158de2183de22470bfc3
Mateo Morandi <114004261+Lucian-Ryu@users.noreply.github.com>
2023-01-25 02:44:30 +0100

    first commit

---
+++ KryptoKidsFund.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract KryptoKidsFund is ReentrancyGuard {
+
+    
+
+    address father;
+
+    event KidAdded(address addr, string firstName, string lastName, uint releaseTime);
+    event DepositToKidDone(address addr, uint amount);
+    event KidWithdrawalComplete(address addr, uint balance);
+
+    constructor() {
+        father = msg.sender;
+    }
+
+    mapping (address => Kid) public kidMapping;
+
+    struct Kid {
+        address payable kidAddress;
+        string firstName;
+        string lastName;
+        uint releaseTime;
+        uint balance;
+    }
+
+    modifier onlyFather() {
+        require(msg.sender == father, "Only the father can add kids");
+        _;
+    }
+
+    function addKid(address payable _kidAddress, string memory firstName, string memory lastName, uint releaseTime) public onlyFather {
+        kidMapping[_kidAddress] = Kid(_kidAddress, firstName, lastName, releaseTime, 0);
+
+        emit KidAdded(_kidAddress, firstName, lastName, releaseTime);
+    }
+
+    function depositToKid(address payable _kidAddress) payable public onlyFather {
+        require(msg.value > 0, "amount should be greater than zero");
+        kidMapping[_kidAddress].balance += msg.value;
+
+        emit DepositToKidDone(_kidAddress, msg.value);
+    }
+
+    function getBalanceOfKid(address _kidAddress) public view returns(uint) {
+        return kidMapping[_kidAddress].balance;
+    }
+
+    function getReleaseTime(address _kidAddress) public view returns(uint){
+        return kidMapping[_kidAddress].releaseTime;
+    }
+
+    function withdraw(address payable _kidAddress) payable public nonReentrant {
+        Kid memory _kid = kidMapping[_kidAddress];
+        require(msg.sender == _kid.kidAddress, "you must be the kid to withdraw");
+        require(block.timestamp > _kid.releaseTime, "you cannot withdraw yet");
+        require(_kid.balance > 0, "balance should be greater than zero");
+        payable(_kid.kidAddress).transfer(_kid.balance);
+        kidMapping[_kidAddress].balance = 0;
+
+        emit KidWithdrawalComplete(_kidAddress,_kid.balance);
+    }
+}
